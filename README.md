009df268abd06e8cc6ab3b295c64f185dcedf334
ERROR404LOVE <87453385+ERROR404LOVE@users.noreply.github.com>
2021-07-17 12:58:26 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+<p>$git clone https://github.com/ERROR404LOVE/billhack.git
+<p>cd billhack
+<p>python billhack.py
---
+++ billhack.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import pyfiglet
+import os
+import time
+import sys
+
+os.system("clear")
+os.system("clear")
+
+print('''\033[96m
+ We Are Myanmar ™
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+slowprint (''' \033[91m
++--------------------------------------+
+bill hack tool''')
+slowprint(''' \033[93m
+[1]billhack''')
+print ("                                            ")
+choice = input("\033[93mDo you hack bill[y/n] :  ")
+if choice == 'n' : sys.exit()
+if choice == 'y' :os.system("apt update")
+ph =input("\033[93m please enter you ph=")
+un =input("\033[93m please enter hack MMK=")
+
+
+slowprint("\033[93m Taw tar")
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[95m+-------------------------------------------------+")
+slowprint('''\033[95m|           #LOVE                         |''')
